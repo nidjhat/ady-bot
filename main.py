b9592c0852e627ebf0c4b615e4d2af0074c5fb6c
nidjhat👁‍🗨 <pnicat20@gmail.com>
2026-06-05 12:47:09 +0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ TOKEN = os.getenv("TOKEN")
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot işləyir 👍")
 
-app = Application.builder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
 
-app.run_polling()
+    print("Bot started")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
